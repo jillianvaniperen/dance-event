2e3bb70505a723a39e404dc27496af51f1edcd0e
jillianvaniperen <jillianvaniperen@gmail.com>
2022-11-16 09:30:39 +0100

    Added the Scanner class to Main

--- DanceEvent/src/Entity/Main.java
+++ DanceEvent/src/Entity/Main.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +8,11 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Choose your option: ");
-
-
+        System.out.println("Choose your option (1-4): " +
+                "\n1. Line up Friday \n" +
+                "2. Line up Saturday \n" +
+                "3. Line up Sunday \n" +
+                "4. Write a review");
+        String option = input.nextLine();
     }
 }
